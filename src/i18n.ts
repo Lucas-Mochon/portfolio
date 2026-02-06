f5ed8bcfe77b2d163d24401f0ad96d0263880238
gorbayotte <mochonlucas@gmail.com>
2026-02-06 13:30:40 +0100

    fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
-import en from "./Locales/en.json"
-import fr from "./Locales/fr.json"
+import en from "./locales/en.json"
+import fr from "./locales/fr.json"
 
 i18n
   .use(initReactI18next)
